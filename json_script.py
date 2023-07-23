af47899700feba43a34c7a724729dafc433e69d1
HirokiShimoi <you@example.com>
2023-07-23 17:58:34 +0900

    0723

--- json_script.py
+++ json_script.py
@@ -3,18 +3,32 @@ import pandas as pd
 # CSVファイルを読み込む
 df = pd.read_csv('./filtered_excel/bcart_products.csv')
 
+df.replace(['\t', '\n', '\r'], ['', '',''], regex=True, inplace=True)
+
 # 同一商品名の行をまとめる
 grouped = df.groupby('【基本】商品名')
 
 # 新たなJSONデータを作成
 new_data = []
 for name, group in grouped:
+    spicy = []
+    if pd.notnull(group['甘辛度1'].iloc[0]):
+        spicy.append(group['甘辛度1'].iloc[0])
+    if pd.notnull(group['甘辛度2'].iloc[0]):
+        spicy.append(group['甘辛度2'].iloc[0])
+
+    smell = []
+    if pd.notnull(group['香り1'].iloc[0]):
+        smell.append(group['香り1'].iloc[0])
+    if pd.notnull(group['香り2'].iloc[0]):
+        smell.append(group['香り2'].iloc[0])
+
     item = {
         'productName': name,
         'maker': group['メーカー名'].iloc[0],
         'region': group['【基本】生産地'].iloc[0],
-        'spicy': group['甘辛度'].iloc[0],
-        'smell': group['香り'].iloc[0],
+        'spicy': spicy,  # Update here
+        'smell': smell,  # Update here
         'spec': group['スペック'].iloc[0],
         'rice': group['酒米'].iloc[0],
         'gift': group['ギフト'].iloc[0],
@@ -35,4 +49,4 @@ for name, group in grouped:
     new_data.append(item)
 
 # JSONファイルとして保存
-pd.DataFrame(new_data).to_json('converted.json', orient='records',force_ascii=False)
+pd.DataFrame(new_data).to_json('converted.json_heiwa', orient='records',force_ascii=False)
--- pandas_script.py
+++ pandas_script.py
@@ -1,35 +1,5 @@
 import pandas as pd
 import os
-<<<<<<< HEAD
-
-# 入力ファイルのディレクトリ
-input_dir = './csv/'
-# 出力ファイルのディレクトリ
-output_dir = './filtered_csv/'
-
-# 入力ファイルのリスト
-input_files = ['bcart_products.csv']  # ここにさらに読み込むファイル名を追加していく
-
-for file_name in input_files:
-    df = pd.read_csv(os.path.join(input_dir, file_name), encoding='shift_jis', 
-                     usecols=['【基本】商品名','【セット】品番', '【基本】キャッチコピー','【基本】説明','【基本】生産地','【基本】画像','【セット】単価'])
-    df = df[['【基本】商品名','【セット】品番', '【基本】生産地', '【セット】単価', '【基本】キャッチコピー','【基本】説明', '【基本】画像']]
-    df.insert(2, 'メーカー名', '平和酒造')  # 適宜、メーカー名を変更する
-    df.insert(4, '甘辛度', '中口')
-    df.insert(5, '香り', 'やや強い')
-    df.insert(6, 'スペック', '純米大吟醸')
-    df.insert(7, 'ギフト', False)
-    df.insert(8, 'クール', False)
-    df.insert(7, '酒米', False)
-    df.insert(8, '在庫', False)
-
-    # ファイル名から拡張子を除去したものを新しい名前とする（例：'bcart_products_heiwa'）
-    new_name = os.path.splitext(file_name)[0]
-    
-    # 新しい名前で出力する
-    df.to_csv(os.path.join(output_dir, new_name + '.csv'), index=False)
-    df.to_excel(os.path.join(output_dir, new_name + '.xlsx'), index=False)
-=======
 
 # 入力ファイルのディレクトリ
 input_dir = './csv/'
@@ -46,20 +16,21 @@ for file_name in input_files:
     
     df = df[['【基本】商品名','【セット】セット名','【セット】品番', '【基本】生産地', '【セット】単価', '【基本】キャッチコピー','【基本】説明', '【基本】画像']]
     df.insert(3, 'メーカー名', '平和酒造')  # 適宜、メーカー名を変更する
-    df.insert(5, '甘辛度', '中口')
-    df.insert(6, '香り', 'やや強い')
-    df.insert(7, 'スペック', '純米大吟醸')
-    df.insert(8, 'ギフト', False)
-    df.insert(9, 'クール', False)
-    df.insert(10, '酒米', '山田錦')
-    df.insert(11, '在庫', True)
+    df.insert(5, '甘辛度1', '中口')
+    df.insert(6, '甘辛度2', '')
+    df.insert(7, '香り1', 'やや強い')
+    df.insert(8, '香り2', '普通')
+    df.insert(9, 'スペック', '純米大吟醸')
+    df.insert(10, 'ギフト', False)
+    df.insert(11, 'クール', False)
+    df.insert(12, 'にごり', False)
+    df.insert(13, '酒米', '山田錦')
+    df.insert(14, '在庫', True)
 
 
     # ファイル名から拡張子を除去したものを新しい名前とする（例：'bcart_products_heiwa'）
     new_name = os.path.splitext(file_name)[0]
     
     # 新しい名前で出力する
-    df.to_excel(os.path.join(output_dir, new_name + '.xlsx'), index=False)
-    df.to_csv(os.path.join(output_dir, new_name + '.csv'), index=False)
-
->>>>>>> 24cdf3e9fd2ab26d8a53e54610201f5d2c134736
+    df.to_excel(os.path.join(output_dir, new_name +'.xlsx'), index=False)
+    #df.to_csv(os.path.join(output_dir, new_name + '.csv'), index=False)
